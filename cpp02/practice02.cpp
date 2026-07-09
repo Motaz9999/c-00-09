6dac13a7fc552558eb1a1ef4f897ab53731840bc
Motaz9999 <motasjamal246@gmail.com>
2026-07-09 18:29:29 +0300

    init some files

---
+++ cpp02/practice02.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   practice02.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moodeh <moodeh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/08 21:24:08 by moodeh            #+#    #+#             */
+/*   Updated: 2026/07/08 22:45:49 by moodeh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+//operators are symbols that tell the compiler to preform specific mathematical, logical, or data-manipulation operations.
+
+//arithmetic ops
+//+ - * % / 
+//assignment ops
+// =   += -=  *= /= %=
+//relational (Comparison) ops
+//==  != > <  <= >=
+//logical ops
+//&& || ! 
+//bitwise ops
+// & | ^ << >> 
+
+//stream io ops
+// << >> 
+//Memory & pointers ops
+//& address * deference new new[] dynamic allocation delete delete[] deallocation -> Member Selection via pointer . Member Selection via Object
+
+//Miscellaneous Operators
+//sizeof() ?:-> condition ? expression_if_true : expression_if_false;
+// , :: 
+
+
+//each operator are a fun
+//like the a + b the + here is operator+ fun 
+
+
+//btw the cpp allows me to define what the fun dose when its operands are your
